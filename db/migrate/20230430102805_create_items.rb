7bcb25722866bd159fe81f41391c4a0ef860dfaa
Bludidi <bludidi5@gmail.com>
2023-04-30 14:00:59 +0200

    Create schema

---
+++ db/migrate/20230430102805_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.string :amount
+      t.timestamps
+    end
+  end
+end
