6c86013f656f15151bc27f5ed6ee834c654eb802
Bludidi <bludidi5@gmail.com>
2023-05-01 13:19:04 +0200

    Add devise gem and configure it

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :items
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
