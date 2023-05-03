3370fb5cbb89036c6c53299cdfd80bd0ff6c667c
Bludidi <bludidi5@gmail.com>
2023-05-03 13:40:04 +0200

    Create item index page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,7 @@ class ItemsController < ApplicationController
     @categories = Category.find_by(id: params[:category_id])
     # @category = @categories.find(params[:category_id])
     @items = ItemCategory.includes(:item).where(category: @categories).order(created_at: :desc)
+    @total = @items.map{ |paid| paid.item.amount }.sum  
   end
 
   # GET /items/new
@@ -14,9 +15,6 @@ class ItemsController < ApplicationController
     @category = @categories.find(params[:category_id])
     @item = Item.new
   end
-
-  # GET /items/1/edit
-  def edit; end
 
   # POST /items or /items.json
   def create
@@ -28,20 +26,7 @@ class ItemsController < ApplicationController
     @item_categories = ItemCategory.new(category_id: @category.id, item_id: @item.id)
     return unless @item_categories.save
 
-    redirect_to item_categories_path(@category)
-  end
-
-  # PATCH/PUT /items/1 or /items/1.json
-  def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to item_url(@item), notice: 'Item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to categories_path
   end
 
   # DELETE /items/1 or /items/1.json
