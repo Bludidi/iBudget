7036548930d17a469a455b4e620661de9a6a6cda
Bludidi <bludidi5@gmail.com>
2023-05-04 15:41:10 +0200

    Fix routing error

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,6 @@ class ItemsController < ApplicationController
   # GET /items or /items.json
   def index
     @categories = Category.find_by(id: params[:category_id])
-    # @category = @categories.find(params[:category_id])
     @items = ItemCategory.includes(:item).where(category: @categories).order(created_at: :desc)
     @total = @items.map{ |paid| paid.item.amount }.sum  
   end
@@ -16,7 +15,6 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  # POST /items or /items.json
   def create
     @category = Category.find(params[:category_id])
     @item = @user.items.new(item_params)
@@ -26,8 +24,27 @@ class ItemsController < ApplicationController
     @item_categories = ItemCategory.new(category_id: @category.id, item_id: @item.id)
     return unless @item_categories.save
 
-    redirect_to categories_path
+    redirect_to category_items_path(@category)
   end
+# ====================================================
+  # POST /items or /items.json
+ # def create
+  #  @category = Category.find(params[:category_id])
+   # @item = @category.items.build(item_params)
+    # @item_categories = ItemCategory.new(category_id: @category.id, item_id: @item.id)
+    
+    #if @item.save 
+    #  @item_categories = ItemCategory.create(category_id: @category.id, item_id: @item.id)
+     # redirect_to categories_path
+    #end
+    
+    #@item_categories = ItemCategory.new(category_id: @category.id, item_id: @item.id)
+   # if @item_categories.save
+
+     # redirect_to categories_path
+    #end
+ # end
+# ===============================================
 
   # DELETE /items/1 or /items/1.json
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_005005) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_110630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_005005) do
   create_table "items", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
-    t.money "amount", scale: 2
+    t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
