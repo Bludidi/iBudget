b4f2fc10ddbdd9e63e6b7324ae6acc3c599ec059
Bludidi <bludidi5@gmail.com>
2023-05-05 22:30:01 +0200

    Fix linter errors

--- spec/feature/categories/index.html.erb_spec.rb
+++ spec/feature/categories/index.html.erb_spec.rb
@@ -6,15 +6,14 @@ RSpec.describe 'categories/index.html.erb', type: :feature do
     visit new_user_session_path
   end
 
- describe 'Login user' do
-  
-  it 'When I fill in correct logging credentials' do
-    click_button 'Log in'
-    expect(page).to have_content("LOGIN")
-  end
+  describe 'Login user' do
+    it 'When I fill in correct logging credentials' do
+      click_button 'Log in'
+      expect(page).to have_content('LOGIN')
+    end
 
-  it 'Option to reset your password is present' do
-    expect(page).to have_content("Forgot your password?")
-  end
+    it 'Option to reset your password is present' do
+      expect(page).to have_content('Forgot your password?')
+    end
   end
 end
