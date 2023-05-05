afde38d80e7ce2954731376f39a80ecc207dba6c
Bludidi <bludidi5@gmail.com>
2023-05-05 16:28:53 +0200

    Add unit tests for models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.create(name: 'Parchie', email: 'parchie@pmail.com', password: '123456')}
+  
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = 'Parchie'
+    expect(subject).to be_valid
+  end
+
+  it 'email should be present' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'password should be present' do
+    #subject.password = nil
+    expect(subject).to_not be_nil
+  end
+end
