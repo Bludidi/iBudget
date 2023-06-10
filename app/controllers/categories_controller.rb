17742abef83587fc1e93f0f9d3ebd096d0ee4c85
Bludidi <bludidi5@gmail.com>
2023-06-10 12:14:17 +0200

    Add a method to add categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,8 +5,8 @@ class CategoriesController < ApplicationController
   def index
     @categories = @user.categories
     @item_categories = ItemCategory.all
-    # @category = Category.find(params[:id])
-    # @icon_path = @category.icon.url
+    @items = @user.items
+    update_total_amount
   end
 
   # GET /categories/1 or /categories/1.json
@@ -38,9 +38,10 @@ class CategoriesController < ApplicationController
   def destroy
     @category = Category.find(params[:id])
     @category.destroy
+    update_total_amount
 
     respond_to do |format|
-      format.html { redirect_to category_path, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to category_path, notice: 'Category was successfully destroyed!' }
       format.json { head :no_content }
     end
   end
@@ -55,5 +56,17 @@ class CategoriesController < ApplicationController
   # Only allow a list of trusted parameters through.
   def category_params
     params.require(:category).permit(:name, :icon)
+  end
+
+  # def delete_item_amount
+  #   @items = @user.items
+  #   @total = @items.map { |item| item.amount }.sum
+  #   if 
+
+  # end
+
+  def update_total_amount
+    @items = @user.items
+    @total = @items.map { |item| item.amount }.sum
   end
 end
